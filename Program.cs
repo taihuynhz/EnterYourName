c6c3f01f7d1e088a69a9cf98a850bef2e2662185
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-01 12:25:56 +0700

    EnterYourName

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterYourName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
